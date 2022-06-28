4a6e93d67e3f7d9b2964d1369498a35b85a600f0
Alextav1 <alextav2010@gmail.com>
2022-06-28 23:36:43 +0300

    3.14 the function runTests() is added

--- nette-blog/app/Presenters/HomepagePresenter.php
+++ nette-blog/app/Presenters/HomepagePresenter.php
@@ -7,6 +7,7 @@ namespace App\Presenters;
 use LengthException;
 use Nette;
 use Nette\Application\UI\Form;
+use Nette\Schema\ValidationException;
 use Nette\Utils\Arrays;
 use Nette\Utils\ArrayHash;
 use Nette\Utils\Callback;
@@ -17,6 +18,33 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
 
 	const SIZE = 10; // the variable that give for us the size of batterfield
 
+
+	public function actionDefault (){
+
+		//$this->runTests();
+
+
+	}
+
+	protected function runTests(){
+
+		if (false !== $this->validateShipLocation([
+			0, 1, 11,
+		])) {
+			echo 'Bug!';
+			die();
+		}
+		
+		if (true !== $this->validateShipLocation([
+			0, 2,
+		])) {
+			echo 'Bug!';
+			die();
+		}
+
+		die();
+
+	}
 	
   protected function createComponentShipsInput(): Form
 	{
@@ -34,40 +62,42 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
 			print_r($form->getValues());
 			$shipsArray = $form->getValues();
 
-			function validateShipLocation($arrayToValidate) {
-
-				$counter = count($arrayToValidate);
-				$flag = true;
-
-				if($counter == 0){
-					echo 'Add the ship!';
-					}
-					elseif($counter == 1) { 
-						echo 'The ship is added!';
-						}
-						else{
-							foreach($arrayToValidate as $key=>$items){
-								foreach($arrayToValidate as $key2=>$items){
-									if($arrayToValidate[$key] != $arrayToValidate[$key2] ) {
-										if ($arrayToValidate[$key] == $arrayToValidate[$key2]+1 && $items%10!=9 ||
-										    $arrayToValidate[$key] == $arrayToValidate[$key2]+9 && $items%10!=0 ||									
-												$arrayToValidate[$key] == $arrayToValidate[$key2]+10 && $items%10!=9 ||																						
-												$arrayToValidate[$key] == $arrayToValidate[$key2]+11 && $items%10!=9 ||
-												$arrayToValidate[$key] == $arrayToValidate[$key2]+10)
-												{
-													$flag = false;													
-												} 
-											}
-										}
-									} 
-									if($flag == false) {
-										echo 'The distance between ships must be one box';
-									}									
-								}
-								return $flag;
-			}
-			 validateShipLocation($shipsArray->ships);
+			 $this->validateShipLocation($shipsArray->ships);
 			};
 			return $form;
+		}
+
+
+		protected function validateShipLocation($arrayToValidate) {
+
+			$counter = count($arrayToValidate);
+			$flag = true;
+
+			if ($counter == 0){
+				echo 'Add the ship!';
+			}
+				elseif ($counter == 1) { 
+					echo 'The ship is added!';
+					}
+					else{
+						foreach($arrayToValidate as $key=>$items){
+							foreach($arrayToValidate as $key2=>$items){
+								if($arrayToValidate[$key] != $arrayToValidate[$key2] ) {
+									if ($arrayToValidate[$key] == $arrayToValidate[$key2]+1 && $items%10!=9 ||
+											$arrayToValidate[$key] == $arrayToValidate[$key2]+9 && $items%10!=0 ||									
+											$arrayToValidate[$key] == $arrayToValidate[$key2]+10 && $items%10!=9 ||																						
+											$arrayToValidate[$key] == $arrayToValidate[$key2]+11 && $items%10!=9 ||
+											$arrayToValidate[$key] == $arrayToValidate[$key2]+10)
+											{
+												$flag = false;													
+											} 
+										}
+									}
+								} 
+								if($flag == false) {
+									echo 'The distance between ships must be one box';
+								}									
+							}
+							return $flag;
 		}
 	}
